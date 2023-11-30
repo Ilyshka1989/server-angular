fe9c0fe7ca846557643580199f2c4d451da9eaf7
DESKTOP-HAERTJK\ilysh <ilyshka19890307@gmail.com>
2023-11-30 20:46:05 +0300

    creating queries

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
+const connectionRoutes = require('./routes/connection')
+const rewiewsRoutes = require('./routes/reviews')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 const app = express();
 
+app.use(morgan('dev'))
+app.use(cors())
+app.use('/api/connection', connectionRoutes)
+app.use('/api/rewiew', rewiewsRoutes)
 
 module.exports = app
