7acb39bdae4e9b9ae3f3ea088e6e3b0b3e6d2f91
DESKTOP-HAERTJK\ilysh <ilyshka19890307@gmail.com>
2023-12-05 20:42:47 +0300

    change project

--- controllers/connection.js
+++
@@ -1,8 +0,0 @@
-module.exports.create = async function (req, res){
-    const connection = new Connection({
-        name: req.body.name,
-        number: req.body.number 
-    })
-    await connection.commit()
-    res.status(200).json(connection)
-}
--- routes/reviews.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const controller = require('../controllers/rewiews')
-const router = express.Router()
-
- router.get('/',  controller.getAll)
-
- router.post('/',  controller.create)
